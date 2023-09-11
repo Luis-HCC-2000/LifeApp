346d2ba13c71847ef562b70e39f9b3cbff6708e4
Luis-HCC-2000 <55676066+Luis-HCC-2000@users.noreply.github.com>
2023-09-11 03:21:16 -0600

    started creating the cbt process

---
+++ fullApp/CBT/steps.js
@@ -0,0 +1,5 @@
+let steps=["Assessment and Goal Setting", "Identification of Cognitive Distortions", "Challenging and Restructuring Thoughts", "Behavioral Experiments and Exposure", "Skill Building and Coping Strategies", "Homework Assignments", "Monitoring Progress", "Termination and Relapse Prevention", "Follow-Up and Support" ]
+
+let asessment={
+    questionary:[]
+}
